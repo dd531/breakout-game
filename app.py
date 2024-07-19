bce6e6ddd19a4d8eeca6c8c87dbce40cda4baabf
dd531 <praidy100@naver.com>
2024-07-19 10:41:44 +0900

    create canvas

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("Hello, World")
+import pygame
+
+pygame.init()
+
+beige_marka = (250, 237, 201)
+
+screen_width = 800
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("벽돌 깨기 게임")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(beige_marka)
+
+    pygame.display.flip()
+
+pygame.quit()
